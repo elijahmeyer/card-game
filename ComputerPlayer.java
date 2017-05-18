53bba5d2607569ad1afcbfe6a250cbea50fcd4e7
elijahmeyer <elijahmeyer59@yahoo.com>
2017-05-18 14:47:45 -0400

    Add files via upload

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -7,6 +7,7 @@
 package project3_meyer;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 
 /**
  * @author Elijah Meyer
@@ -30,27 +31,251 @@ public class ComputerPlayer extends Player
     public int promptForDrawInput(ArrayDeque<Card> discard)
     {
         
-        // Choose value and return
-        return (int)(Math.random() * 2) + 1;
+        int discardMatch = 0;
+        for (int i = 0; i < this.getHand().size(); i++) 
+        {
+            if (this.getHand().get(i).equals(discard.peek()))
+            {
+                discardMatch++;
+            }
+        }
+        
+        if (discardMatch == 0)
+        {
+            return 2;
+        }
+                
+        if (discardMatch == 1)
+        {
+            int handMatch = 0;
+            for (int i = 0; i < this.getHand().size() - 1; i++)
+            {
+                if (this.getHand().get(i).equals(this.getHand().get(i + 1)))
+                {
+                    handMatch++;
+                }
+            }
+            
+            if (handMatch == 3) 
+            {
+                return 2;
+            }
+        }
+        
+        return 1;
     }
 
     /**
      * Chooses which Card the calling ComputerPlayer will discard.
-     * Precondition: discard is initialized
+     * Precondition: none
      * Postcondition: an integer reflecting which Card the ComputerPlayer will discard
      * will have been returned
-     * @param discard - the ArrayDeque where the Players have been storing their discarded Cards
      * @return the ComputerPlayer's choice of what Card to discard
      */
     @Override
-    public int promptForDiscardInput(ArrayDeque<Card> discard)
+    public int promptForDiscardInput()
     {
         
-        // Choose value
-        int randomVar = (int)(Math.random() * 5) + 1;
+        // Initialize variables
+        int valueToReturn = 0;
+        int matchCount = 0;
         
-        // Output decision and return
-        System.out.println("I will discard the " + this.getHand().get(randomVar - 1));
-        return randomVar;
+        // Compare each Card in hand with the other Cards and record number of matches
+        for (int i = 0; i < this.getHand().size(); i++)
+        {
+            for (int j = i; j < this.getHand().size(); j++)
+            {
+                if (this.getHand().get(i).equals(this.getHand().get(j)) && i != j)
+                {
+                    matchCount++;
+                }
+            }
+        }
+        
+        // If there is at least one match...
+        if (matchCount >= 1)
+        {
+            
+            // Initialize variables
+            Card c1 = this.getHand().get(0);
+            Card c2 = this.getHand().get(1);
+            Card c3 = this.getHand().get(2);
+            Card c4 = this.getHand().get(3);
+            Card c5 = this.getHand().get(4);
+            
+            // If there is one match...
+            if (matchCount == 1)
+            {
+                
+                // Compare each Card in hand to the other Cards, and record value of matching Cards
+                int value = 0;
+                for (int i = 0; i < this.getHand().size(); i++)
+                {
+                    for (int j = 0; j < this.getHand().size(); j++)
+                    {
+                        if (this.getHand().get(i).equals(this.getHand().get(j)) && i != j)
+                        {
+                            value = this.getHand().get(i).getValue();
+                        }
+                    }
+                }
+                
+                // Find last Card in hand that does not match, and set valueToReturn to reference of that Card
+                for (int i = 0; i < this.getHand().size(); i++) 
+                {
+                    if (this.getHand().get(i).getValue() != value) 
+                    {
+                        valueToReturn = i + 1;
+                    }
+                }
+            }
+            
+            // If there are two pairs...
+            else if (matchCount == 2)
+            {
+                
+                // Count number of Cards that match first Card in hand
+                int c1MatchCount = 0;
+                for (int i = 1; i < this.getHand().size(); i++)
+                {
+                    if (c1.equals(this.getHand().get(i)))
+                    {
+                        c1MatchCount++;
+                    }
+                }
+                
+                // If no Cards match, set valueToReturn to 1 (Card 1 is odd man out) 
+                if (c1MatchCount == 0)
+                {
+                    valueToReturn = 1;
+                }
+               
+                else 
+                {
+                    
+                    // Count number of Cards that match second Card in hand
+                    int c2MatchCount = 0;
+                    for (int i = 0; i < this.getHand().size(); i++)
+                    {
+                        if (c2.equals(this.getHand().get(i)) && i != 1)
+                        {
+                            c2MatchCount++;
+                        }
+                    }
+                    
+                    // If Card 2 is odd man out, set valueToReturn to 2
+                    if (c2MatchCount == 0)
+                    {
+                        valueToReturn = 2;
+                    }
+                    
+                    else
+                    {
+                        
+                        // If Card 1 and Card 2 match...
+                        if (c1.equals(c2))
+                        {
+                            
+                            // Check if Card 3 is odd man out, and if so, set valueToReturn to 3
+                            if (!c3.equals(c4) && !c3.equals(c5))
+                            {
+                                valueToReturn = 3;
+                            }
+                            
+                            // If not, set valueToReturn to reference of Card that does not match Card 3
+                            else 
+                            {
+                                for (int i = 3; i < this.getHand().size(); i++)
+                                {
+                                    if (!c3.equals(this.getHand().get(i))) 
+                                    {
+                                        valueToReturn = i + 1;
+                                    }
+                                }
+                            }
+                        }
+                        
+                        // If Card 1 and Card 2 do not match, set valueToReturn to reference of Card that matches neither of them
+                        else 
+                        {
+                            for (int i = 0; i < this.getHand().size(); i++)
+                            {
+                                if (!c1.equals(this.getHand().get(i)) && !c2.equals(this.getHand().get(i)))
+                                {
+                                    valueToReturn = i + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            // If there are three matching Cards, regardless of whether the other two match...
+            else if (matchCount == 3 || matchCount == 4)
+            {
+                
+                // Check how many Cards match first Card in hand
+                int c1MatchCount = 0;
+                for (int i = 1; i < this.getHand().size(); i++) 
+                {
+                    if (c1.equals(this.getHand().get(i)))
+                    {
+                        c1MatchCount++;
+                    }
+                }
+                
+                // If Card 1 is not part of the three of a kind, set valueToReturn to 1
+                if (c1MatchCount < 2)
+                {
+                    valueToReturn = 1;
+                }
+                
+                else
+                {
+                    
+                    // Set valueToReturn to the reference of the last Card that did not match Card 1
+                    int value = c1.getValue();
+                    for (int i = 1; i < this.getHand().size(); i++) 
+                    {
+                        if (this.getHand().get(i).getValue() != value) 
+                        {
+                            valueToReturn = i + 1;
+                        }
+                    }
+                }
+            }
+            
+            // If four Cards match...
+            else if (matchCount == 6)
+            {
+                
+                // If first Card in hand does not match two other Cards, set valueToReturn to 1 (Card 1 is odd man out)
+                if (!c1.equals(c2) && !c1.equals(c3))
+                {
+                    valueToReturn = 1;
+                }
+                
+                // Otherwise, set valueToReturn to reference of Card that does not match Card 1
+                int value = c1.getValue();
+                for (int i = 1; i < this.getHand().size(); i++)
+                {
+                    if (this.getHand().get(i).getValue() != value)
+                    {
+                        valueToReturn = i + 1;
+                    }
+                }
+            }
+        }
+        
+        // If there are no matches...
+        else
+        {
+            // Set valueToReturn to random value
+            valueToReturn = (int)(Math.random() * 5) + 1;
+        }
+        
+        // Output decision and return chosen value
+        System.out.println("I will discard the " + this.getHand().get(valueToReturn - 1));
+        return valueToReturn;
     }
 }
